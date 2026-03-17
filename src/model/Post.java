f4a2606d2c8252515590ca17d1707e48176c1818
dzemar21 <dzemar21@gmail.com>
2026-03-17 11:45:01 +0200

    Post class added

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,40 @@
 package model;
 
-public class Post {
+import java.time.LocalDateTime;
 
+public class Post {
+	// 1. lokalie mainigie
+	private String msg;
+	private LocalDateTime dateTime = LocalDateTime.now();
+	private int countOfLikes = 0;
+	
+	// 2. un 3. getters un setters
+	
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		if((msg != null) && (!msg.isEmpty()) && (msg.length() < 1000)) {
+			this.msg = msg;
+		}else {
+			this.msg = "Unknown post";
+		}
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime() {
+		dateTime = LocalDateTime.now();
+	}
+	public int getCountOfLikes() {
+		return countOfLikes;
+	}
+	public void incrementCountOfLikes() {
+		countOfLikes++;
+	}
+	
+	// 4. non args
+	// 5. args
+	// 6. toString
+	// 7. others
 }
